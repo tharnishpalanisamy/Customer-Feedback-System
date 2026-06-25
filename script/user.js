022a2857b84d3c9cb4014024d729dfd2e4e0fe7c
Tharnish Palanisamy <tharnishpalanisamy3@gmail.com>
2026-06-25 14:12:15 +0530

    user feedback filtering

--- script/user.js
+++ script/user.js
@@ -38,17 +38,45 @@ if(!user) {
     window.location.href('../index.html')
 }
 
-console.log(user.name);
-console.log(user.id);
 let firstName = document.getElementById('name').value
 document.getElementById('name').innerText = `Welcome ${user.name} ,`
+
+
+//showing statistics
+async function displayStatistics(){
+    let feedbackData = await fetch(`${FEEDBACKAPI}?userId=${user.id}`) 
+    let feedbacks = await feedbackData.json()  
+
+    let totalFeedback = feedbacks.length 
+    let totalResponse = 0 
+    let totalRating = 0 
+
+    feedbacks.forEach(feedback =>{
+        totalRating += Number(feedback.rating)
+        if(feedback.status == 'Responded') {
+            totalResponse++
+        }
+    })
+
+    document.querySelector('.feedbackCount').innerText = totalFeedback 
+    document.querySelector('.responseCount').innerText = totalResponse 
+    document.querySelector('.averageRating').innerText = Math.round(totalRating / totalFeedback) 
+    document.querySelector('.responseRate').innerText = Math.round((totalResponse / totalFeedback ) * 100) + ' %'
+}
+
+displayStatistics()
+
+
+
+
+
 
 //adding feeback 
 
 //validating feedback 
 
-function validateFeedback(title , feedback , rating){
-    if(!title || !feedback || !rating ) {
+function validateFeedback(title , feedback , rating , department){
+    if(!title || !feedback || !rating || !department ) {
         alert('Fields cannot be empty') 
         return false 
     }
@@ -61,18 +89,20 @@ saveFeedbackBtn.addEventListener('click' , async  function(){
     let title = document.getElementById('title') 
     let feedback = document.getElementById('feedback') 
     let rating = document.getElementById('rating')
-    let valid = validateFeedback(title.value , feedback.value , rating.value)
+    let department = document.getElementById('department') 
+    let valid = validateFeedback(title.value , feedback.value , rating.value , department.value)
     if(valid) {
         let feebackData = { 
             userId:user.id,
             username:user.name,
             email:user.email,
-            title:title.value , 
+            title:title.value ,
+            department:department.value, 
             feedback:feedback.value , 
             rating : rating.value , 
             createdOn : new Date().toISOString() , 
             active:1 , 
-            remarks:"" , 
+            remarks:"-" , 
             status:'Pending' 
         }
 
@@ -89,6 +119,7 @@ saveFeedbackBtn.addEventListener('click' , async  function(){
         title.value = '' 
         feedback.value = '' 
         rating.value = ''
+        displayStatistics()
         let modalElement = document.getElementById('feedbackModal') 
         let modal = bootstrap.Modal.getInstance(modalElement) 
         modal.hide()
@@ -97,4 +128,14 @@ saveFeedbackBtn.addEventListener('click' , async  function(){
 
     
 
+})
+
+
+
+//dynamic view / filtering based on user choice
+
+let totalView = document.getElementById('totalView') 
+totalView.addEventListener('click' , function(){
+    localStorage.setItem('status','All')
+    window.location.href = './userfeedback.html'
 })
--- script/userfeedback.js
+++ script/userfeedback.js
@@ -2,8 +2,6 @@ import { USERSAPI , FEEDBACKAPI } from './api.js';
 
 
 //theme
-
-
 //if user previously has dark theme
 
 if(localStorage.getItem('theme') == 'dark') {
@@ -31,7 +29,8 @@ theme.addEventListener('click', changeTheme)
 theme2.addEventListener('click', changeTheme)
 
 
-
+//user
+let user = JSON.parse(localStorage.getItem('user'))
 
 //creating feedback 
 
@@ -56,11 +55,19 @@ function createFeedback(feedbacks){
             <td>${index+1}</td>
             <td>${feedback.title}</td>
             <td>${stars}</td>
+            <td>${feedback.department}</td>
             <td>${feedback.feedback}</td>
             <td class = '${feedback.status} text'>${feedback.status}</td>
             <td>${createdDate.getDate()}-${createdDate.getMonth()}-${createdDate.getFullYear()}</td>
             <td class = 'text-center'>${feedback.respondedOn?feedback.respondedOn:'-'}</td> 
-            <td >View</td>
+            <td class = 'text-center'>${feedback.remarks}</td>
+            <td class='text-center' >
+            <i class="bi bi-pencil-square fs-5  ${feedback.status == 'Pending' ? 'active editFeedbackIcon' : 'blocked'}" 
+            ${feedback.status=='Pending' ? ` data-bs-toggle="modal" data-bs-target="#editFeedbackModal" 
+                data-id = ${feedback.id}  ` : ''}
+            ></i>
+            </td>
+            
         </tr>
         `
     })
@@ -72,8 +79,136 @@ function createFeedback(feedbacks){
 async function displayFeedback(){
     let feedbackData = await fetch(FEEDBACKAPI) 
     let feedbacks = await feedbackData.json() 
-
-    createFeedback(feedbacks)
+    let userFeedbacks = feedbacks.filter(feedback => feedback.userId = user.id)
+    userFeedbacks.sort((a,b)=>{
+        return new Date(b.createdOn) - new Date(a.createdOn) 
+    })
+    createFeedback(userFeedbacks)
 }
 
 displayFeedback()
+
+
+//filtering 
+
+
+let departmentFilter = document.getElementById('departmentFilter') 
+let statusFilter = document.getElementById('statusFilter')
+let searchTitle = document.getElementById('searchTitle') 
+
+
+async function filterFeedback(){
+    let dept = departmentFilter.value;
+    let status = statusFilter.value;
+    let value = searchTitle.value.toLowerCase();
+
+    let response = await fetch(`${FEEDBACKAPI}?userId=${user.id}`);
+    let feedbacks = await response.json();
+
+    if (dept !== 'All') {
+        feedbacks = feedbacks.filter(feedback => feedback.department === dept);
+    }
+
+    if (status !== 'All') {
+        feedbacks = feedbacks.filter(feedback => feedback.status === status);
+    }
+
+    if (value !== '') {
+        feedbacks = feedbacks.filter(feedback => feedback.title.toLowerCase().includes(value));
+    }
+    feedbacks.sort((a,b) =>{
+        return new Date(b.createdOn) - new Date(a.createdOn)
+    })
+    createFeedback(feedbacks);
+
+}
+
+//dept
+departmentFilter.addEventListener('change' , async function(){
+    await filterFeedback()
+    searchTitle.value = ''
+})
+//status
+statusFilter.addEventListener('change' , async function(){
+    await filterFeedback()
+    searchTitle.value = ''
+})
+
+//search 
+
+searchTitle.addEventListener('input' ,async function(){
+    await filterFeedback()
+
+    
+})
+
+
+//edit icon in action 
+let currentId = ''
+
+
+//elements of edit modal 
+let editTitle = document.getElementById('editTitle') 
+let editDepartment = document.getElementById('editDepartment') 
+let editFeedback = document.getElementById('editFeedback')
+let editRating = document.getElementById('editRating') 
+
+
+document.addEventListener('click' , async  function(event){
+    if(event.target.classList.contains('editFeedbackIcon')){
+        currentId = event.target.dataset.id         
+        
+        let feedbackData = await fetch(`${FEEDBACKAPI}?id=${currentId}`) 
+        let feedback = await feedbackData.json() 
+
+        let data = feedback[0] 
+
+        editTitle.value = data.title 
+        editDepartment.value = data.department 
+        editRating.value = data.rating  
+        editFeedback.value = data.feedback
+    }
+})
+
+
+//Feedback Validation
+function validateFeedback(title , feedback , rating , department){
+    if(!title || !feedback || !rating || !department ) {
+        alert('Fields cannot be empty') 
+        return false 
+    }
+    return true 
+}
+
+//saving edited feedback 
+let saveBtn = document.getElementById('saveBtn') 
+saveBtn.addEventListener('click' , async function(){
+    let isValid = validateFeedback(editTitle.value , editFeedback.value , editRating.value , editDepartment.value) 
+
+    if(isValid) {
+        let editedFeedback = {
+            title:editTitle.value , 
+            department:editDepartment.value , 
+            rating:editRating.value , 
+            feedback:editFeedback.value , 
+            updatedOn:new Date().toISOString()
+        }
+
+        await fetch(`${FEEDBACKAPI}/${currentId}` , {
+            method:"PATCH" , 
+            headers : {
+                'Content-type' : 'application/json'
+            } , 
+            body:JSON.stringify(editedFeedback)
+        })
+    }
+    await filterFeedback()
+
+
+
+    //closing the modal 
+    let modalElement = document.getElementById('editFeedbackModal') 
+    let modal = bootstrap.Modal.getInstance(modalElement) 
+    modal.hide()
+    
+})
